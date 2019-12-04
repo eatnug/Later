5ae12b2abeb22531372905ebf46698e79f7bcd56
eatnug <wlq4568@gmail.com>
2019-12-04 20:32:49 +0900

    feat: Article Component
    Add 버튼으로 Article 크롬 스토리지에 저장하고, App 컴포넌트에서 hook을 이용해서 스토리지에 저장된 아이템을 가져오고 출력해준다.
    문제가 하나 있는데, useEffect에서 setArticles로 맨 처음 익스텐션 켰을때 스토리지 값을 불러오는데 이게 계속 반복되어서 최초에만 실행되게 했다.
    그래서 새로 url을 추가해서 state가 변해도 로드하지 않는다. 다시 켜야한다 창을

--- build/background.js
+++ build/background.js
@@ -1,3 +1,10 @@
+  
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.sync.set({ list: [{title:"Later", url:"https://github.com/EatNug/Later", isRead:false, tags:["developer"], id:0}] }, function() {
+    console.log("list is set to initial state.");
+  });
+});
+
 
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
